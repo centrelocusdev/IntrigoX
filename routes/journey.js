86c05d46f48d9e862f058bdfd9905aa70e57837d
Bhavya Tiwari <bhavyatiwari917@gmail.com>
2023-11-28 13:00:17 +0530

    update journey module

--- routes/journey.js
+++ routes/journey.js
@@ -1,8 +1,9 @@
 const express= require('express')
 const router = express.Router()
 const journey = require('../controllers/journey');
+const authentication = require('../middleware/authentication');
 
 router.post('/languageConvert' , journey.languageConvert);
-router.get('/getChaptersData' , journey.getChaptersData);
+router.get('/getChaptersData' , authentication.auth, journey.getChaptersData);
 router.get('/storeChaptersData' , journey.storeChaptersData);
 module.exports = router
